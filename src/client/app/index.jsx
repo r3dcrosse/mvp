3fedce76ba98696ba40d93751dba63e54a6c1033
David Wayman <mrdavidwayman@gmail.com>
2016-12-05 17:38:35 -0800

    Added pure.css, adjusting layout

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import {render} from 'react-dom';
 import Tweets from './Tweets.jsx';
 import Nav from './Nav.jsx';
+import SideBar from './SideBar.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   render () {
     return (
       <div>
-        <Nav />
-        <div className="col-md-7">
+        <SideBar className="pure-u-1-2"/>
+        <div className="pure-g">
+          <Nav className="pure-u-1"/>
+          <div className="pure-u-1-2">
           <Tweets tweetData={window.dummyTweetsData.statuses} />
+          </div>
         </div>
       </div>
     );
--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -65,6 +65,10 @@
 	
 	var _Nav2 = _interopRequireDefault(_Nav);
 	
+	var _SideBar = __webpack_require__(/*! ./SideBar.jsx */ 181);
+	
+	var _SideBar2 = _interopRequireDefault(_SideBar);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -88,11 +92,16 @@
 	      return _react2.default.createElement(
 	        'div',
 	        null,
-	        _react2.default.createElement(_Nav2.default, null),
+	        _react2.default.createElement(_SideBar2.default, { className: 'pure-u-1-2' }),
 	        _react2.default.createElement(
 	          'div',
-	          { className: 'col-md-7' },
-	          _react2.default.createElement(_Tweets2.default, { tweetData: window.dummyTweetsData.statuses })
+	          { className: 'pure-g' },
+	          _react2.default.createElement(_Nav2.default, { className: 'pure-u-1' }),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'pure-u-1-2' },
+	            _react2.default.createElement(_Tweets2.default, { tweetData: window.dummyTweetsData.statuses })
+	          )
 	        )
 	      );
 	    }
@@ -22287,6 +22296,133 @@
 	}(_react2.default.Component);
 	
 	exports.default = Nav;
+
+/***/ },
+/* 181 */
+/*!************************************!*\
+  !*** ./src/client/app/SideBar.jsx ***!
+  \************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var SideBar = function (_React$Component) {
+	    _inherits(SideBar, _React$Component);
+	
+	    function SideBar(props) {
+	        _classCallCheck(this, SideBar);
+	
+	        return _possibleConstructorReturn(this, (SideBar.__proto__ || Object.getPrototypeOf(SideBar)).call(this, props));
+	    }
+	
+	    _createClass(SideBar, [{
+	        key: "render",
+	        value: function render() {
+	            return _react2.default.createElement(
+	                "div",
+	                null,
+	                _react2.default.createElement(
+	                    "ul",
+	                    { className: "sidebar-nav" },
+	                    _react2.default.createElement(
+	                        "li",
+	                        { className: "sidebar-brand" },
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "Start Bootstrap"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "li",
+	                        null,
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "Dashboard"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "li",
+	                        null,
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "Shortcuts"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "li",
+	                        null,
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "Overview"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "li",
+	                        null,
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "Events"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "li",
+	                        null,
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "About"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "li",
+	                        null,
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "Services"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "li",
+	                        null,
+	                        _react2.default.createElement(
+	                            "a",
+	                            { href: "#" },
+	                            "Contact"
+	                        )
+	                    )
+	                )
+	            );
+	        }
+	    }]);
+	
+	    return SideBar;
+	}(_react2.default.Component);
+	
+	exports.default = SideBar;
 
 /***/ }
 /******/ ]);
